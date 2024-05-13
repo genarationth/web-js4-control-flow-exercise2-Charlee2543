4c07724637f2b5e5fb62fef719e2f2ab1852ad65
Charlee2543 <Charlee2543@hotmail.com>
2024-05-13 17:32:14 +0700

    Ex2-switch

---
+++ Ex2_useSwitchCase.js
@@ -0,0 +1,26 @@
+const Ex2_day=20;
+switch(true){
+    case Ex2_day===0:
+        console.log("sunday");
+        break;
+        case Ex2_day===1:
+        console.log("monday");
+        break;
+        case Ex2_day===2:
+        console.log("tuesday");
+        break;
+        case Ex2_day===3:
+          console.log("wednesday");
+          break;
+          case Ex2_day===4:
+              console.log("thursday");
+              break;
+              case Ex2_day===5:
+                  console.log("friday");
+                  break;
+                  case Ex2_day===6:
+                      console.log("saturday");
+                      break;
+                      default:
+                          console.log("Invalid day number");
+}
